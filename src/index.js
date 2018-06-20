e7667b1efb1c5428394f849b0d9af2b4dd36fc09
drmats <drmats@users.noreply.github.com>
2018-06-20 20:25:34 +0200

    Playground enabled.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,34 @@
  * @license Apache-2.0
  */
 
+import {
+    asyncRepeat,
+    delay,
+    draw,
+    timeUnit,
+} from "@xcmats/js-toolbox"
 
 
 
+
+// ...
+const
+    toy = document.getElementById("toy"),
+    emojis = [
+        "🎁", "🎀", "😊", "🍺", "💣", "💥", "🔥", "👊",
+        "🦊", "👻", "🎶", "🍕", "🚀", "😂", "⚡", "⭐️",
+    ]
+
+
+// ...
 console.log("Hi there... 🌴")
+
+
+// ...
+asyncRepeat(
+    async () => {
+        toy.innerHTML = draw(emojis)
+        await delay(timeUnit.second * 0.5)
+    },
+    () => true
+)
