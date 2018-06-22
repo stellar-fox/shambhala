b222086fa90dcba1f105add76cb75969357db58e
drmats <drmats@users.noreply.github.com>
2018-06-22 21:51:35 +0200

    service worker: events handling (debug messages).

--- src/index.sw.js
+++ src/index.sw.js
@@ -10,6 +10,10 @@
 
 import { api } from "./env"
 import { console } from "./utils"
+import {
+    delay,
+    timeUnit,
+} from "@xcmats/js-toolbox"
 
 
 
@@ -23,8 +27,39 @@ print.info("Honored to see you, I am. Hmmmmmm. 🍄")
 
 
 // handle "install" event
-self.addEventListener("install", (_e) => {
-    print.info("Installing myself...")
+self.addEventListener("install", (installEvent) => {
+    print.info("<install>", "Installing myself...")
+    self.skipWaiting()
+    installEvent.waitUntil((async () => {
+        await delay(2 * timeUnit.second)
+        print.info("<install>", "Finished, I have.")
+        // throw new Error("Simulated failure")
+    })())
+})
+
+
+// handle "activate" event
+self.addEventListener("activate", (_activateEvent) => {
+    self.clients.claim()
+    print.info("Activated, I am.")
+})
+
+
+// handle "online" event
+self.addEventListener("online", () => {
+    print.info("Online now, I become.")
+})
+
+
+// handle "offline" event
+self.addEventListener("offline", () => {
+    print.info("Offline now, I become.")
+})
+
+
+// handle "error" event
+self.addEventListener("error", (e) => {
+    print.info("Terrible thing happened:", e)
 })
 
 
@@ -37,6 +72,7 @@ self.addEventListener("fetch", (e) => {
             { status: 200, }
         ))
     } else {
+        print.info(e.request.method, e.request.url)
         e.respondWith(fetch(e.request))
     }
 })
