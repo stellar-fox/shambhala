32b1ae256040b77a7831dad912a04bc6b2153509
drmats <drmats@users.noreply.github.com>
2018-09-24 15:46:09 +0200

    Backend: 'credentials' file added.

---
+++ backend.src/credentials.default.js
@@ -0,0 +1,18 @@
+/**
+ * Shambhala backend - credentials.
+ *
+ * @module @stellar-fox/shambhala
+ * @license Apache-2.0
+ */
+
+
+
+
+// ...
+export const database = {
+    user: "user",
+    pass: "pass",
+    host: "localhost",
+    port: "5432",
+    db: "db",
+}
