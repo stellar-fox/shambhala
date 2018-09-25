8f0a89c758e6fb66ff7dc9300de613580f156308
drmats <drmats@users.noreply.github.com>
2018-09-25 23:31:21 +0200

    cryptops: genUUID() added.

--- src/cryptops.js
+++ src/cryptops.js
@@ -8,7 +8,12 @@
 
 
 
-import { emptyString } from "@xcmats/js-toolbox"
+import {
+    emptyString,
+    handleException,
+    isBrowser,
+    range,
+} from "@xcmats/js-toolbox"
 import crypto from "crypto-browserify"
 import sjcl from "sjcl"
 
@@ -75,3 +80,48 @@ export const genRandomSHA256 = () =>
     sjcl.codec.hex.fromBits(
         sjcl.hash.sha256.hash(sjcl.random.randomWords(16))
     )
+
+
+
+
+/**
+ * Generate 128 bits UUID. Comprised of:
+ * - 48 bits of miliseconds since epoch
+ * - 32 bits of truncated SHA256 sum of userAgent string
+ * - 48 random bits
+ *
+ * @function genUUID
+ * @returns {String}
+ */
+export const genUUID = () => {
+    let rd = Date.now().toString(16).split("").reverse()
+    return (
+        // 48 bits (6 bytes): timestamp - miliseconds since epoch
+        range(6*2)
+            .reduce(
+                (acc, _el, i) =>
+                    i < rd.length ?
+                        [rd[i],].concat(acc) :
+                        [0,].concat(acc),
+                []
+            ).join("")
+    ) + (
+        // 32 bits (4 bytes): truncated SHA256 sum of userAgent string
+        sjcl.codec.hex.fromBits(
+            sjcl.hash.sha256.hash(
+                sjcl.codec.utf8String.toBits(
+                    handleException(
+                        () => isBrowser() ?
+                            navigator.userAgent :
+                            "non-browser-env"
+                    )
+                )
+            )
+        ).slice(0, 4*2)
+    ) + (
+        // 48 random bits (6 bytes)
+        sjcl.codec.hex.fromBits(
+            sjcl.random.randomWords(2)
+        ).slice(0, 6*2)
+    )
+}
