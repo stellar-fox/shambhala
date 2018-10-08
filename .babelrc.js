05146b5c76f35d9ca707dd60597209bf543aadb0
drmats <drmats@users.noreply.github.com>
2018-10-08 17:00:03 +0200

    Config: babel - restored env. for 'devApiServer'.

--- .babelrc.js
+++ .babelrc.js
@@ -1,27 +1,35 @@
+// used babel-plugins array
+var plugins = [
+    "@babel/plugin-proposal-object-rest-spread",
+    "@babel/plugin-syntax-dynamic-import",
+    "@babel/plugin-transform-arrow-functions",
+    "@babel/plugin-transform-block-scoped-functions",
+    "@babel/plugin-transform-block-scoping",
+    "@babel/plugin-transform-computed-properties",
+    "@babel/plugin-transform-destructuring",
+    "@babel/plugin-transform-exponentiation-operator",
+    "@babel/plugin-transform-literals",
+    "@babel/plugin-transform-parameters",
+    "@babel/plugin-transform-runtime",
+    "@babel/plugin-transform-shorthand-properties",
+    "@babel/plugin-transform-spread",
+    "@babel/plugin-transform-template-literals"
+];
+
+
+
+
+// configuration
 module.exports = function (api) {
     api.cache.using(() => process.env.BABEL_ENV);
     console.log("Compiling for", "'" + api.env() + "'", "...");
 
     return {
         "env": {
+            // shambhala-frontend environment (dev.)
             "development": {
                 "comments": false,
-                "plugins": [
-                    "@babel/plugin-proposal-object-rest-spread",
-                    "@babel/plugin-syntax-dynamic-import",
-                    "@babel/plugin-transform-arrow-functions",
-                    "@babel/plugin-transform-block-scoped-functions",
-                    "@babel/plugin-transform-block-scoping",
-                    "@babel/plugin-transform-computed-properties",
-                    "@babel/plugin-transform-destructuring",
-                    "@babel/plugin-transform-exponentiation-operator",
-                    "@babel/plugin-transform-literals",
-                    "@babel/plugin-transform-parameters",
-                    "@babel/plugin-transform-runtime",
-                    "@babel/plugin-transform-shorthand-properties",
-                    "@babel/plugin-transform-spread",
-                    "@babel/plugin-transform-template-literals"
-                ],
+                "plugins": plugins,
                 "presets": [
                     [
                         "@babel/preset-env",
@@ -31,6 +39,21 @@ module.exports = function (api) {
                             "targets": {
                                 "esmodules": true
                             }
+                        }
+                    ]
+                ]
+            },
+
+            // shambhala-backend environment (devApiServer and production)
+            "commonjs": {
+                "comments": false,
+                "plugins": plugins,
+                "presets": [
+                    [
+                        "@babel/preset-env",
+                        {
+                            "modules": "commonjs",
+                            "shippedProposals": true
                         }
                     ]
                 ]
