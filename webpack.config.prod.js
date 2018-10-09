26ba1649a9aae5a11dbe41c58abd198fb6bad03d
drmats <drmats@users.noreply.github.com>
2018-10-09 18:30:27 +0200

    Config: webpack: added production-build config.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -7,8 +7,6 @@
 const
     fs = require("fs"),
     path = require("path"),
-    express = require("express"),
-    chalk = require("chalk"),
     webpack = require("webpack"),
     HtmlWebpackPlugin = require("html-webpack-plugin"),
     appDirectory = fs.realpathSync(process.cwd()),
@@ -23,7 +21,7 @@ const
 // ...
 module.exports = {
 
-    mode: "development",
+    mode: "production",
 
 
     entry: {
@@ -43,6 +41,7 @@ module.exports = {
         filename: "[name].[hash].bundle.js",
         chunkFilename: "[name].[hash].chunk.js",
         sourceMapFilename: "[name].[hash].map",
+        path: path.resolve(__dirname, "dist"),
         publicPath,
         globalObject: "self",
     },
@@ -63,50 +62,14 @@ module.exports = {
     },
 
 
-    devServer: {
-        before: function (app) {
-            // static-files server
-            app.use(
-                path.join(publicPath, publicDirectory),
-                express.static(path.join(appDirectory, publicDirectory))
-            )
-
-            // info
-            console.log(
-                chalk.gray(" [info]") + ":",
-                "Static output is served from",
-                chalk.bold.blue(publicDirectory),
-                "as",
-                chalk.bold.blue(path.join(publicPath, publicDirectory))
-            )
-
-            // simple logger
-            // app.use(function (req, _res, next) {
-            //     console.log(req.method, req.url)
-            //     next()
-            // })
-        },
-        compress: true,
-        disableHostCheck: true,
-        host: "0.0.0.0",
-        hot: true,
-        https: false,
-        index: "index.html",
-        inline: true,
-        open: false,
-        overlay: true,
-        port: 8080,
-        public: "localhost:8080",
-        publicPath,
-        useLocalIp: true,
-        watchContentBase: true,
-    },
-
-
     devtool: "source-map",
 
 
     plugins: [
+
+        new webpack.DefinePlugin({
+            "process.env.BABEL_ENV": JSON.stringify("production"),
+        }),
 
         new webpack.HotModuleReplacementPlugin(),
 
