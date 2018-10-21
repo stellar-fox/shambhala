e833e60362004578c2042a953866178606afaa04
drmats <drmats@users.noreply.github.com>
2018-10-21 10:33:19 +0200

    Server: GENERATE_SIGNING_KEYS - SQL file added.

---
+++ src/server/generate_signing_keys.sql
@@ -0,0 +1,12 @@
+-- signing keys generation
+
+
+-- store S_PUBLIC and ENC_SKP in a row
+-- with appropriate G_PUBLIC with C_UUID
+UPDATE $<key_table:name>
+SET
+    enc_skp = $<ENC_SKP>,
+    datum = jsonb_build_object(
+        's_public', $<S_PUBLIC>
+    )
+WHERE g_public = $<G_PUBLIC> AND c_uuid = $<C_UUID>;
