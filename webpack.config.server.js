09dffecf14446d574488188b352d2f813b022ec6
drmats <drmats@users.noreply.github.com>
2018-10-23 18:39:23 +0200

    Config: webpack - enabled TreeShaking for apiServer production builds.

--- webpack.config.server.js
+++ webpack.config.server.js
@@ -48,6 +48,7 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: ["babel-loader"],
+                sideEffects: false,
             },
         ],
     },
