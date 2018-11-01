aaf6ca31fa74bff100c8a0f02aa7c5cd685b4ffa
drmats <drmats@users.noreply.github.com>
2018-11-01 22:10:07 +0100

    Host: onboarding - '[v. A]' - all steps done.

--- src/host/index.js
+++ src/host/index.js
@@ -10,10 +10,17 @@
 
 
 
-import { Networks } from "stellar-sdk"
+import axios from "axios"
+import {
+    Network,
+    Networks,
+    Server,
+    Transaction,
+} from "stellar-sdk"
 import {
     async,
     devEnv,
+    func,
     isObject,
     randomInt,
     string,
@@ -48,20 +55,20 @@ window.addEventListener("load", async () => {
     logger.info("Hi there! 🌴")
 
 
-    // do something...
+    // entertain...
     const toy = document.getElementById("toy")
     async.repeat(async () => {
-        toy.innerHTML = drawEmojis(randomInt() % 4 + 1).join(" ")
-        await async.delay(timeUnit.second * 0.8)
+        toy.innerHTML = drawEmojis(randomInt() % 4 + 1).join(string.space())
+        await async.delay(0.8 * timeUnit.second)
     }, () => true)
 
 
     // expose `s` dev. namespace
     if (devEnv()  &&  isObject(window)) {
-        window.s = {
-            ...await dynamicImportLibs(),
-        }
+        window.s = { ...await dynamicImportLibs() }
     }
+
+
 
 
     // do meaningful stuff
@@ -73,53 +80,119 @@ window.addEventListener("load", async () => {
 
         // Onboarding - [Variant A]
 
+
+        // choose network and _stellar_ horizon server
+        Network.use(new Network(Networks.TESTNET))
+        let server = new Server("https://horizon-testnet.stellar.org/")
+
+
+
+
+        // account generation
+        // https://bit.ly/shambhalagenaccount
         logger.info("Requesting address generation...")
+
         let G_PUBLIC = await shambhala.generateAccount()
+
         logger.info("Got it:", string.shorten(G_PUBLIC, 11))
+        await async.delay(0.1 * timeUnit.second)
 
-        await async.delay(timeUnit.second)
 
+
+
+        // signing keys generation
+        // https://bit.ly/shambhalagensig
         logger.info("Requesting signing keys generation...")
+
         let { C_PUBLIC, S_PUBLIC } =
             await shambhala.generateSigningKeys(G_PUBLIC)
+
         logger.info(
             "Got them:",
             string.shorten(C_PUBLIC, 11),
             string.shorten(S_PUBLIC, 11)
         )
+        await async.delay(0.1 * timeUnit.second)
 
-        await async.delay(timeUnit.second)
 
+
+
+        // account creation and initial funding
+        // https://friendbot.stellar.org/
         logger.info("Requesting account generation and initial funds...")
-        ;((x) => (x))(G_PUBLIC)  // no-op
-        await async.delay(0.3 * timeUnit.second)
-        logger.warn("NOT IMPLEMENTED.")
 
-        await async.delay(timeUnit.second)
+        let friendbotResponse =
+            await axios.get("https://friendbot.stellar.org/", {
+                params: { addr: G_PUBLIC },
+            })
 
-        logger.info("Getting account information (current sequence)...")
-        ;((x) => (x))(G_PUBLIC)  // no-op
-        let sequence = string.random(11, string.digits())
-        await async.delay(0.2 * timeUnit.second)
-        logger.warn("NOT IMPLEMENTED.")
+        logger.info(
+            "Got it:",
+            func.compose(
+                string.quote,
+                (op) => `${op.type}: ${op.startingBalance} XLM`,
+                (tx) => tx.operations[0],
+                (xdr64) => new Transaction(xdr64)
+            )(friendbotResponse.data.envelope_xdr)
+        )
+        await async.delay(0.1 * timeUnit.second)
 
-        await async.delay(timeUnit.second)
 
-        logger.info("Receiving transaction associating keys with account...")
+
+
+        // find sequence number
+        // http://bit.ly/stellarseqnumber
+        logger.info("Getting account sequence...")
+
+        let sequence = (
+            await server.loadAccount(G_PUBLIC)
+        ).sequenceNumber()
+
+        logger.info("It's:", string.quote(sequence))
+        await async.delay(0.1 * timeUnit.second)
+
+
+
+
+        // automatic keys association
+        // http://bit.ly/shambhalaautokeyassoc
+        logger.info("Requesting transaction associating keys with account...")
+
         let tx = await shambhala.generateSignedKeyAssocTX(
             G_PUBLIC, sequence, Networks.TESTNET
         )
-        logger.info("It came:", tx)
 
-        await async.delay(timeUnit.second)
+        logger.info(
+            "It came:",
+            func.compose(
+                string.quote,
+                (opTypes) => opTypes.join(string.space()),
+                (ops) => ops.map((op) => op.type)
+            )(tx.operations)
+        )
+        await async.delay(0.1 * timeUnit.second)
 
+
+
+
+        // send transaction to the network
+        // https://bit.ly/stellarsubmittx
         logger.info("Sending transaction to the stellar network.")
-        ;((x) => (x))(tx)  // no-op
-        await async.delay(0.5 * timeUnit.second)
+
+        await server.submitTransaction(tx)
+
         logger.info("Sent.")
+        await async.delay(0.1 * timeUnit.second)
+
+
+
+
+        logger.info("All done!")
 
     } catch (ex) {
+
         logger.error("Whoops...", ex)
+
     }
 
 })
