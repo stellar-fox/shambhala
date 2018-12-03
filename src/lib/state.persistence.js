696408ce1e46038220dcc5c288916d5f3a3098b8
drmats <drmats@users.noreply.github.com>
2018-12-03 14:47:45 +0100

    Client: functions for persisting redux state in sessionStorage added.

---
+++ src/lib/state.persistence.js
@@ -0,0 +1,50 @@
+/**
+ * Shambhala.
+ *
+ * Frontend environment variables.
+ *
+ * @module state-persistence
+ * @license Apache-2.0
+ */
+
+
+
+
+import {
+    type,
+    utils,
+} from "@xcmats/js-toolbox"
+import { ssAppStateKey } from "../config/frontend"
+
+
+
+
+/**
+ * Restore current Redux state from `sessionStorage`.
+ *
+ * @function loadState
+ */
+export const loadState = () =>
+    utils.handleException(
+        () => type.nullToUndefined(
+            JSON.parse(sessionStorage.getItem(ssAppStateKey))
+        ),
+        // eslint-disable-next-line no-console
+        (ex) => type.nullToUndefined(console.log(ex))
+    )
+
+
+
+
+/**
+ * Persists current state of the application.
+ *
+ * @function saveState
+ * @param {Object} state
+ */
+export const saveState = (state) =>
+    utils.handleException(
+        () => sessionStorage.setItem(ssAppStateKey, JSON.stringify(state)),
+        // eslint-disable-next-line no-console
+        console.log
+    )
