29b73c642a20f87cfde9c301e4b5fa63036d859e
drmats <drmats@users.noreply.github.com>
2018-12-18 07:26:48 +0100

    utils: jss color helpers.

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -136,3 +136,34 @@ export const run = (main) => {
         window.addEventListener("load", main)
     }
 }
+
+
+
+
+/**
+ * JSS color.
+ *
+ * @function rgb
+ * @param {Number} r
+ * @param {Number} g
+ * @param {Number} b
+ * @returns {String}
+ */
+export const rgb = (r, g, b) =>
+    string.wrap([r, g, b].join(", "), "rgb(", ")")
+
+
+
+
+/**
+ * JSS color (with alpha).
+ *
+ * @function rgba
+ * @param {Number} r
+ * @param {Number} g
+ * @param {Number} b
+ * @param {Number} a
+ * @returns {String}
+ */
+export const rgba = (r, g, b, a) =>
+    string.wrap([r, g, b, a].join(", "), "rgba(", ")")
